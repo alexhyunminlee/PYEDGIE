00d911aaecea348f19c0fabc0e4d704e1acba18b
Zach <zach@ZipZap-MacBook.local>
2025-06-25 20:58:25 -0400

    added in trirnd function

--- utils/distributions.py
+++ utils/distributions.py
@@ -12,6 +12,8 @@ INVALID_BOUNDS_MSG = (
 )
 INVALID_STD_DEV_MSG = "gauss: Standard deviation should be a positive number"
 MISSING_BOUNDS_MSG = "gauss: Both a and b must be provided when mean or std_dev are not specified."
+TRIRND_INVALID_BOUNDS_MSG = "trirnd: The upper bound must be strictly greater than the lower bound"
+TRIRND_INVALID_PEAK_MSG = "trirnd: The peak must be between the lower and upper bound, inclusive."
 
 
 def gauss(m: int, n: int, a: Optional[float] = None, b: Optional[float] = None, mean=None, std_dev=None):
@@ -50,3 +52,35 @@ def gauss(m: int, n: int, a: Optional[float] = None, b: Optional[float] = None, 
     # Generate a random sample from the Gaussian distribution with specified mean and std_dev
     np.random.seed(seed=int(time.time()))
     return np.random.normal(loc=mean, scale=std_dev, size=(m, n)).tolist()
+
+
+def trirnd(a: float, c: float, m: int, n: int, mode=None):
+    """
+    Generate a random array of size m by n sampled from a Gaussian distribution.
+
+    Parameters:
+    - a: The lower-bound cutoff for the distribution sampling
+    - c: The upper-bound cutoff for the distribution sampling
+    - m: The number of rows in the output array.
+    - n: The number of columns in the output array.
+
+    Parameters (optional):
+    - mode: the peak of the triangular distribution
+
+    Returns:
+    - An m x n matrix with values sampled from the triangular distribution.
+
+    Authors: Priyadarshan (priyada@purdue.edu), Alex Lee (alexlee5124@gmail.com)
+    Date: 12/12/2024
+    """
+    if a >= c:
+        raise ValueError(TRIRND_INVALID_BOUNDS_MSG)
+
+    if mode is None:
+        mode = (a + c) / 2.0
+    if (mode < a) or (mode > c):
+        raise ValueError(TRIRND_INVALID_PEAK_MSG)
+
+    # Generate a random sample from a triangular distribution with specified lower bound, mode, and upper bound
+    np.random.seed(seed=int(time.time()))
+    return np.random.triangular(a, mode, c, size=(m, n)).tolist()
