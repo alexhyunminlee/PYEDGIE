68074985eb73ab844d50a73e9da17684a9a92c15
Zach <zach@ZipZap-MacBook.local>
2025-07-14 15:41:17 -0400

    csv to dict test

---
+++ tests/test_fileIO.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+from utils import fileIO
+
+def test_csv_to_dict():
+    file_path = "data/TexasWeather1.csv"
+    result = fileIO.csv_to_dict(file_path)
+    assert len(result) == 8
+    assert result[0]['coordinates (lat,lon)'] == "(31.9973, -102.0779)"
+    assert result[0]['model (name)'] == "era5"
+    assert result[0]['temperature (degC)'] == "22.95"
+    assert result[0]['wind_speed (m/s)'] == "4.19"
+    assert result[0]['relative_humidity (0-1)'] == "0.66"
+    # You can add more asserts for other columns or rows as needed
