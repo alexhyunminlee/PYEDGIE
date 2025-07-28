f05413fa4a6225167198dacdd922a1c152bf87ce
Zach <zach@ZipZap-MacBook.local>
2025-07-27 22:32:07 -0400

    Fix: formatting issues in utils/__init__.py (trailing whitespace, EOF newline)

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,12 +3,10 @@ Utility functions for PYEDGIE.
 
 This package contains various utility modules:
 - conversions: Unit conversion functions
-- distributions: Statistical distribution functions  
+- distributions: Statistical distribution functions
 - fileIO: File input/output operations
 """
 
-from . import conversions
-from . import distributions
-from . import fileIO
+from . import conversions, distributions, fileIO
 
-__all__ = ["conversions", "distributions", "fileIO"] 
+__all__ = ["conversions", "distributions", "fileIO"]
